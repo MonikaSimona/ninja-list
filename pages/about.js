267b3aeb8c53d29ee0cdbe160fef280125d85520
Simona Simonovska <56088273+MonikaSimona@users.noreply.github.com>
2021-02-09 17:30:50 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+          <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+        <h1>About</h1>
+        </>
+     );
+}
+ 
+export default About;
